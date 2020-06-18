c394e9256fc90d3b517f0968aed762cbb22e94d7
Michael Chuprin <mkchuprin@gmail.com>
2020-06-18 15:44:21 -0400

    update setup.py to install packages

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='BestPix',
-    version='1.0',
+    name='bestpix',
+    version='1.1',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     scripts=['scripts/reveal'],
@@ -11,5 +11,6 @@ setup(
     author='Michael Chuprin',
     author_email='mkchuprin@gmail.com',
     url='https://github.com/mkchuprin/BestPix',
-    install_requires=['Flask==1.1.2', 'pyheif==0.5.0', 'Pillow==7.1.2']
+    install_requires=['Flask==1.1.2', 'pyheif==0.4', 'Pillow==7.1.2'],
+    packages=['bestpix']
 )
