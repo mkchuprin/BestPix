25861f8afb5a372497413364343f22bd9ab02289
Michael Chuprin <mkchuprin@gmail.com>
2020-06-19 12:15:55 -0400

    specify python version

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='bestpix',
-    version='1.4.1',
-    long_description_content_type='text/markdown',
-    long_description=open('README.md').read(),
-    scripts=['scripts/reveal', 'scripts/cleanup'],
-    license='GNU GPLv3',
+    version='1.4.2',
     author='Michael Chuprin',
     author_email='mkchuprin@gmail.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/mkchuprin/BestPix',
+    scripts=['scripts/reveal', 'scripts/cleanup'],
+    license='GNU GPLv3',
     packages=['bestpix'],
     include_package_data=True,
+    python_requires='>=3',
 )
