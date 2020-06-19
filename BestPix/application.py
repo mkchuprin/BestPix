a41f00db03699f0b4d2a2f45ed333d3b3e360000
Michael Chuprin <mkchuprin@gmail.com>
2020-06-18 23:32:43 -0400

    add main()

--- BestPix/application.py
+++ BestPix/application.py
@@ -83,6 +83,6 @@ def home():
     return render_template('home.html', scores_and_photos = prepared_photos)
 
 
-if __name__ == '__main__':
+def main():
     app.run(debug=False, host="localhost", port=8442) 
 
