b563460e8392ff8209e6face0cef543da09fd671
Michael Chuprin <mkchuprin@gmail.com>
2020-06-21 12:17:14 -0400

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bestpix',
-    version='1.4.8',
+    version='1.4.9',
     author='Michael Chuprin',
     author_email='mkchuprin@gmail.com',
     long_description=open('README.md').read(),
